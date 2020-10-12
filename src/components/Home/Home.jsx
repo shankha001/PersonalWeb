fa553fcc0ea69c5557789598705d8b041f82b490
shankha001 <shankhasubhrabiswas@gmail.com>
2020-10-12 20:22:15 +0530

    [Navbar] add navbar component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './home.styles.scss';
+function Home() {
+  return (
+    <div className="home__container">
+      <div>Home</div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './navbar.styles.scss';
+
+function Navbar() {
+  return (
+    <div className="sidebar__container">
+      <div>Item1</div>
+      <div>Item2</div>
+    </div>
+  );
+}
+
+export default Navbar;
