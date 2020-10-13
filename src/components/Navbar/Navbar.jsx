019fafb8df3e871d4ab1c2acfa2a22831a784e07
shankha001 <shankhasubhrabiswas@gmail.com>
2020-10-13 22:47:56 +0530

    [NAVBAR] add navbar icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import './navbar.styles.scss';
-
+import HomeIcon from '@material-ui/icons/HomeOutlined';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import FlareIcon from '@material-ui/icons/Flare';
+import WorkIcon from '@material-ui/icons/WorkOutlineOutlined';
+import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendarOutlined';
 function Navbar() {
   return (
     <div className="sidebar__container">
-      <div>Item1</div>
-      <div>Item2</div>
+      <div className="sidebar__nav">
+        <div className="sidebar__nav-icon">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="sidebar__nav-icon">
+          <PersonOutlineIcon fontSize="large" />
+        </div>
+        <div className="sidebar__nav-icon">
+          <FlareIcon fontSize="large" />
+        </div>
+        <div className="sidebar__nav-icon">
+          <WorkIcon fontSize="large" />
+        </div>
+        <div className="sidebar__nav-icon">
+          <PermContactCalendarIcon fontSize="large" />
+        </div>
+      </div>
     </div>
   );
 }
